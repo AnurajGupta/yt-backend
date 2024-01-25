159d45937d2a9ce713956bc38963615aae034cb6
Anuraj Gupta <gupta.anuraj109@gmail.com>
2024-01-25 16:03:16 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 // require('dotenv').config({path : './env'});
 import dotenv from "dotenv";
-dotenv.config({path:'./env'});
-
-
-import express from "express";
-const app = express();
-
-
+dotenv.config({ path: "./env" });
 
 import connectDB from "./db/index.js";
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch();
 
 // (async () => {
 //   try {
