f01325740adeeb73a820fd67ee5c334944b142cd
Anuraj Gupta <gupta.anuraj109@gmail.com>
2024-04-27 02:59:37 +0530

    publish video controller added.

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,6 +3,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
 import mongoose, { isValidObjectId } from "mongoose";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 // const getAllVideos = asyncHandler(async (req, res) => {});
 
@@ -20,9 +22,9 @@ const getVideoById = asyncHandler(async (req, res) => {
   const video = await Video.aggregate([
     {
       $match: {
-        _id: new mongoose.Types.ObjectId(videoId), 
+        _id: new mongoose.Types.ObjectId(videoId),
       },
-    }, 
+    },
     {
       $lookup: {
         from: "likes",
@@ -127,4 +129,114 @@ const getVideoById = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, video[0], "video details fetched successfully"));
 });
 
-export { getVideoById };
+const publishVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // check if user is logged in.
+
+  const videoFileLocalPath = req.files?.videoFile[0].path;
+  const thumbnailLocalPath = req.files?.videoFile[0].path;
+
+  if (!videoFileLocalPath) {
+    throw new ApiError(400, "Video file local path is required");
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Thumbnail file local path is required");
+  }
+
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!videoFile) {
+    throw new ApiError(400, "Video file not found");
+  }
+
+  if (!thumbnail) {
+    throw new ApiError(400, "Thumbnail not found");
+  }
+
+  const video = await Video.create({
+    title,
+    description,
+    videoFile: {
+      url: videoFile.url,
+      public_id: videoFile.public_id,
+    },
+    thumbnail: {
+      url: thumbnail.url,
+      public_id: thumbnail.public_id,
+    },
+    duration: videoFile.duration,
+    owner: req.user?._id,
+    isPublished: false,
+  });
+
+  const isUploaded = await Video.findById(video._id);
+
+  if (!isUploaded) {
+    throw new ApiError(
+      500,
+      "There was an error publishing the video.Please try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video Published Successfully"));
+});
+
+// update
+
+// delete
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+  const videoId = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const video = await video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  if (video?.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(400, "Unauthorized User");
+  }
+
+  const togglePublishStatus = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        isPublished: !video?.isPublished,
+      },
+    },
+    { new: true } //if you don't specify { new: true }, Mongoose returns the original document before it was updated.
+  );
+
+  if (!togglePublishStatus) {
+    throw new ApiError(
+      500,
+      "Failed to toggle video publish status. Please try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { isPublished: togglePublishStatus.isPublished },
+        "Video publish status toggled successfully."
+      )
+    );
+});
+
+export { getVideoById, publishVideo ,togglePublishStatus};
